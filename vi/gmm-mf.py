299e16b698fff052db5b690d060f69ff1a0884e8
Sharad Vikram <sharad.vikram@gmail.com>
2017-08-01 21:38:22 -0700

    added in kl divergence calculation

--- vi/gmm-mf.py
+++ vi/gmm-mf.py
@@ -4,7 +4,7 @@ sns.set_style('white')
 import numpy as np
 
 from deepx import T
-from deepx.stats import Gaussian, Dirichlet, NIW, Categorical
+from deepx.stats import Gaussian, Dirichlet, NIW, Categorical, kl_divergence
 
 def generate_data(N, D, K, sigma0=10, sigma=10, seed=None):
     np.random.seed(seed)
@@ -39,7 +39,8 @@ def iter():
     sess.run([z_update])
     sess.run([pi_update])
     sess.run([theta_update])
-    elbos.append(sess.run(elbo))
+    e = sess.run(elbo)
+    elbos.append(e)
     draw()
 
 
@@ -59,9 +60,9 @@ D = 2
 
 sigma = 0.5
 sigma0 = 100
-X = generate_data(N, D, K, sigma=sigma, sigma0=sigma0, seed=4)
+X = generate_data(N, D, K, sigma=sigma, sigma0=sigma0, seed=None)
 p_pi = Dirichlet(T.constant(10.0 * np.ones([K], dtype=T.floatx())))
-p_theta = NIW(map(lambda x: T.constant(np.array(x).astype(T.floatx())), [np.eye(D) * sigma, np.zeros(D), 1, D + 1]))
+p_theta = NIW(list(map(lambda x: T.constant(np.array(x).astype(T.floatx())), [np.eye(D) * sigma, np.zeros(D), 1, D + 1])))
 prior = (p_pi, p_theta)
 
 np.random.seed(None)
@@ -81,33 +82,41 @@ x_tmessage = NIW.pack([
     T.ones(N),
     T.ones(N),
 ])
+x_stats = Gaussian.pack([
+    T.outer(X, X),
+    X,
+])
 theta_cmessage = q_theta.expected_sufficient_statistics()
 
 new_pi = p_pi.get_parameters('natural') + T.sum(z_pmessage, 0)
 parent_pi = p_pi.get_parameters('natural')
 pi_update = T.assign(q_pi.get_parameters('natural'), new_pi)
-l_pi = T.sum(new_pi * parent_pi) + Dirichlet(parent_pi, parameter_type='natural').log_z() \
-                                 - Dirichlet(new_pi, parameter_type='natural').log_z()
+l_pi = T.sum(kl_divergence(q_pi, p_pi))
 
 new_theta = T.einsum('ia,ibc->abc', z_pmessage, x_tmessage) + p_theta.get_parameters('natural')[None]
 parent_theta = p_theta.get_parameters('natural')
 theta_update = T.assign(q_theta.get_parameters('natural'), new_theta)
-l_theta = T.sum(new_theta * parent_theta) + T.sum(K * NIW(parent_theta, parameter_type='natural').log_z()
-                                 - NIW(new_theta, parameter_type='natural').log_z())
+l_theta = T.sum(kl_divergence(q_theta, p_theta))
 
 parent_z = q_pi.expected_sufficient_statistics()[None]
 new_z = T.einsum('iab,jab->ij', x_tmessage, theta_cmessage) + q_pi.expected_sufficient_statistics()[None]
 new_z = new_z - T.logsumexp(new_z, -1)[..., None]
 z_update = T.assign(q_z.get_parameters('natural'), new_z)
-l_z = T.sum(new_z * parent_z) + T.sum(N * Categorical(parent_z, parameter_type='natural').log_z()
-                                 - Categorical(new_z, parameter_type='natural').log_z())
+l_z = T.sum(kl_divergence(q_z, Categorical(parent_z, parameter_type='natural')))
 
-elbo = l_theta + l_pi + l_z
+x_param = T.einsum('ia,abc->ibc', q_z.expected_sufficient_statistics(), q_theta.expected_sufficient_statistics())
+q_x = Gaussian(x_param, parameter_type='natural')
+l_x = T.sum(q_x.log_likelihood(X))
+
+elbo = l_theta + l_pi + l_z + l_x
 elbos = []
 
 sess = T.interactive_session()
 
 draw()
-for i in range(40):
+e = sess.run(elbo)
+print(e)
+print(sess.run([l_pi, l_theta, l_z, l_x]))
+for i in range(100):
     input()
     iter()
